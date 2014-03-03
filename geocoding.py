c0a62361c02805daa8fa83ced9f057dee292d773
DWu39 <dwu399@gmail.com>
2014-03-02 22:45:26 -0500

    added script for geocoding

---
+++ geocoding.py
@@ -0,0 +1,25 @@
+import simplejson, urllib
+
+KEY = "AIzaSyAAYP3vtxZ59mAZ2n2pjXEi4SuhERu3K34" #not used
+GEOCODE_BASE_URL = 'http://maps.googleapis.com/maps/api/geocode/json?address='
+
+addresses = "" #name of file of human-readable addresses
+results = "geocodes.txt"
+
+fp2 = open(results, "w")
+
+with open(addresses) as f:
+	for line in f:
+		# example line:
+		# line = "121 Oxenbridge Rd, Quincy, MA"
+		#format line into url
+		array = line.split(' ')
+		query = '+'.join(array)
+		url = GEOCODE_BASE_URL+query+"&sensor=false"
+		#make API call
+		result = simplejson.load(urllib.urlopen(url))
+		coords = 'lat:%f, lng:%f' %(result['results'][0]['geometry']['location']['lat'], result['results'][0]['geometry']['location']['lng'])
+		#write to fp2
+		fp2.write((line, coords))
+
+fp2.close()
